50e553c7af18678a5c8a8b4b30a6721ec6364255
chenzhicheng <jkt@chenzhicheng.local>
2016-04-01 17:58:01 +0800

    Initial Commit

---
+++ TableViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewDemo
+//
+//  Created by jkt on 16/4/1.
+//  Copyright © 2016年 czc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableViewDemo
+//
+//  Created by jkt on 16/4/1.
+//  Copyright © 2016年 czc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
